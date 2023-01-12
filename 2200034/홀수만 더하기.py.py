c0f719e8ab575bdded6eaaceaee3b9786636f2da
simjunbo2 <junbo2@naver.com>
2023-01-12 17:04:10 +0900

    2200034

---
+++ 2200034/홀수만 더하기.py.py
@@ -0,0 +1,33 @@
+
+
+# import sys
+# sys.stdin = open("#1.txt")
+
+# T = int(input())
+# lists = []
+# for i in range(1,T + 1):
+#     test_Line = list(input().split())
+#     for t in test_Line:
+#         if int(t) % 2 == 1:
+#             lists.append(t)
+#     print(f'#{i} {sum(map(int,lists))}')
+#     lists = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
