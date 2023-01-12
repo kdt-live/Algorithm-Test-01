5a307692a28a4eda6824c2cb599599150cd1bdf0
Lightness-of-Being <gje960217@naver.com>
2023-01-12 17:29:14 +0900

    2200095_곽재은

--- 2200095/새로운 불면증 치료법.py
+++ 2200095/새로운 불면증 치료법.py
@@ -1,4 +1,18 @@
-input_int = int(input())
-input_str = str(input())
+input_str = input()
+input_int = int(input_str)
+res = [
+
+]
+cnt = 1
+while(True):
+    for i in str(input_int * cnt):
+        res.append(i)
+        list(set(res))
+        cnt += 1
+        if len(res) == 10:
+            break
+
+        print(cnt)
+print(res)
 
 
